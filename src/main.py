5b49c302b8cf5576b6bf5c05810a5610e8931925
ad0maa <adam.tunchay@me.com>
2022-11-04 12:54:23 +1100

    alch,marsh & psy install and config

--- src/main.py
+++ src/main.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from init import db, ma, bcrypt, jwt
+import os
+
 
 
 def create_app():
@@ -7,5 +10,13 @@ def create_app():
     @app.route("/")
     def hello_world():
         return "Hello, World!"
+
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+
+    db.init_app(app)
+    ma.init_app(app)
+    jwt.init_app(app)
 
     return app
