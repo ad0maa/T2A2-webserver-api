ddc6078a61ce8dc3393a28e57c675f4b10f72faf
ad0maa <adam.tunchay@me.com>
2022-11-09 11:28:34 +1100

    volume and price endpoints added

--- src/controllers/product_controller.py
+++ src/controllers/product_controller.py
@@ -5,15 +5,15 @@ from sqlalchemy import and_
 
 product_bp = Blueprint('product', __name__, url_prefix='/product')
 
+
+# Returns all products in database
 @product_bp.route('/all', methods=['GET'])
 def view_all():
-
-    # stmt = db.select(Product)
     stmt = db.select(Product)
     products = db.session.scalars(stmt)
     return ProductSchema(many=True).dump(products)
 
-
+# Returns product by id
 @product_bp.route('/<int:id>', methods=['GET'])
 def search_id(id):
     stmt = db.select(Product).filter_by(id=id)
@@ -24,19 +24,7 @@ def search_id(id):
         return {'error': f'No item found with id {id}'}, 404
 
 
-
-#Search for products by minimum length - returns all surfboards that are longer than length provided by client
-# @product_bp.route('/length/<int:length>', methods=['GET'])
-# def search_length(length):
-#     stmt = db.select(Product).filter(and_(Product.length >= length))
-#     product = db.session.scalars(stmt)
-#     if product:
-#         return ProductSchema(many=True).dump(product)
-#     else:
-#         return {'error': f'No item found with id {id}'}, 404
-
-
-#Search for products by minimum length - returns all surfboards that are longer than length provided by client
+#Search for products by length
 @product_bp.route('/length/<string:query>/<int:length>', methods=['GET'])
 def search_length(query,length):
     if query == 'min':
@@ -55,10 +43,39 @@ def search_length(query,length):
         return ProductSchema(many=True).dump(product)
 
 
+# Search for products by volume 
+@product_bp.route('/volume/<string:query>/<int:volume>', methods=['GET'])
+def search_volume(query,volume):
+    if query == 'min':
+        stmt = db.select(Product).filter(and_(Product.volume >= volume))
+        product = db.session.scalars(stmt)
+        return ProductSchema(many=True).dump(product)
 
-    # stmt = db.select(Product).filter_by(Product.length)
-    # product = db.session.scalars(stmt)
-    # if product:
-    #     return ProductSchema(many=True).dump(product)
-    # else:
-    #     return {'error': f'No item found with id {id}'}, 404
+    elif query == 'max':
+        stmt = db.select(Product).filter(and_(Product.volume <= volume))
+        product = db.session.scalars(stmt)
+        return ProductSchema(many=True).dump(product)
+
+    elif query == 'exact':
+        stmt = db.select(Product).filter(and_(Product.volume == volume))
+        product = db.session.scalars(stmt)
+        return ProductSchema(many=True).dump(product)
+
+
+# Search for products by price 
+@product_bp.route('/price/<string:query>/<int:price>', methods=['GET'])
+def search_price(query,price):
+    if query == 'min':
+        stmt = db.select(Product).filter(and_(Product.price >= price))
+        product = db.session.scalars(stmt)
+        return ProductSchema(many=True).dump(product)
+
+    elif query == 'max':
+        stmt = db.select(Product).filter(and_(Product.price <= price))
+        product = db.session.scalars(stmt)
+        return ProductSchema(many=True).dump(product)
+
+    elif query == 'exact':
+        stmt = db.select(Product).filter(and_(Product.price == price))
+        product = db.session.scalars(stmt)
+        return ProductSchema(many=True).dump(product)
--- src/models/review.py
+++ src/models/review.py
@@ -2,8 +2,8 @@ from init import db, ma
 from marshmallow import fields
 from sqlalchemy import ForeignKey
 
-class Review(db.Model):
+# class Review(db.Model):
 
 
 
-    user = db.relationship('User', back_populates='reviews')
+#     user = db.relationship('User', back_populates='reviews')
